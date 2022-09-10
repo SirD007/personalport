2db8edb67724b27ad9e39b4301434d43dd1a63b1
SirD007 <thedavid2009@gmail.com>
2022-09-10 19:37:24 -0300

    added form confirm msg

--- script.js
+++ script.js
@@ -16,16 +16,13 @@ function mobileMenu() {
   }
 
   function sendEmail(){
-  //  const formMsg = document.querySelector('.form_msg').value
+   const formMsg = document.querySelector('.form_msg')
    const formName = document.querySelector('#form_name').value
    const formEmail = document.querySelector('#form_mail').value
    const formSubject = document.querySelector('#tell_more').value
 
     Email.send({
       SecureToken: "8ebdf653-8eaa-4d60-b7a7-e13fdbbdb324",
-      // Host : "smtp.elasticemail.com",
-      // Username : "thedavid2009@gmail.com",
-      // Password : "EF81124380727A601C322B82356CADE25257",
       To : 'thedavid2009@gmail.com',
       From : formEmail,
       Subject : "Um novo contato em seu site [David Morais]",
@@ -33,10 +30,9 @@ function mobileMenu() {
       + "<br> Email: " + formEmail
       + "<br> Message: " + formSubject
   }).then(
-    message => alert(message)
-  //   formMsg.innerHTML = "Message Sent Successfully"
-  // ).catch(
-  //   formMsg.style.color="red",
-  //   formMsg.innerHTML = "Something gone wrong"
+    formMsg.innerHTML = "Message Sent Successfully"
+  ).catch(
+    formMsg.style.color="red",
+    formMsg.innerHTML = "Something gone wrong"
   );
   }
